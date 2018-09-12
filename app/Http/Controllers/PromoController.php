0fe2cec68ec24b2c0c73c1cf24a2dd1d6ef9df56
Arief Muarrief <arhief32@gmail.com>
2018-09-12 11:39:01 +0700

    fix url of photo promo

--- app/Http/Controllers/PromoController.php
+++ app/Http/Controllers/PromoController.php
@@ -23,8 +23,22 @@ class PromoController extends Controller
             $result = DB::connection('school-gateway')->table('promo')->select('*')
             ->where('active', 1)
             ->get();
+
+            $result_v2 = [];
+            foreach($result as $promo)
+            {
+                array_push($result_v2, [
+                    'promoID' => $promo->promoID,
+                    'type' => $promo->type,
+                    'photo' => 'http://'.$_SERVER['HTTP_HOST'].'/PUB_IMAGE/promo/'.$promo->type.'/'.$promo->photo,
+                    'info' => $promo->info,
+                    'active' => $promo->active,
+                    'title' => $promo->title,
+                    'date' => $promo->date,
+                ]);
+            }
             
-            return response()->json(ResponseCode::success($result));
+            return response()->json(ResponseCode::success($result_v2));
         }
     }
 }
