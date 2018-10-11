c775663e10d1fd18ea93c6be502bca9d972a3b1a
Arief Muarrief Buhari <arhief32@gmail.com>
2018-10-11 15:05:08 +0700

    change promo to WS-Promo

--- app/Http/Controllers/PromoController.php
+++ app/Http/Controllers/PromoController.php
@@ -20,25 +20,44 @@ class PromoController extends Controller
         else
         {
             // "SELECT * FROM `promo`;
-            $result = DB::connection('school-gateway')->table('promo')->select('*')
-            ->where('active', 1)
-            ->get();
+            // $result = DB::connection('school-gateway')->table('promo')->select('*')
+            // ->where('active', 1)
+            // ->get();
 
-            $result_v2 = [];
-            foreach($result as $promo)
+            // $result_v2 = [];
+            // foreach($result as $promo)
+            // {
+            //     array_push($result_v2, [
+            //         'promoID' => $promo->promoID,
+            //         'type' => $promo->type,
+            //         'photo' => 'http://172.18.133.135:81/PUB_IMAGE/promo/'. strtolower($promo->type) . '/'.$promo->photo,
+            //         'info' => $promo->info,
+            //         'active' => $promo->active,
+            //         'title' => $promo->title,
+            //         'date' => $promo->date,
+            //     ]);
+            // }
+
+            $client = new \GuzzleHttp\Client(['http_errors' => false]);
+            $get_promos = $client->get(env('PROMO_URL').'/get-promo?platform_id=3')->getBody();
+            $promos = json_decode($get_promos);
+            $promos = $promos->responseData;
+            
+            $result = [];
+            foreach($promos as $promo)
             {
-                array_push($result_v2, [
-                    'promoID' => $promo->promoID,
-                    'type' => $promo->type,
-                    'photo' => 'http://172.18.133.135:81/PUB_IMAGE/promo/'. strtolower($promo->type) . '/'.$promo->photo,
-                    'info' => $promo->info,
-                    'active' => $promo->active,
+                array_push($result, [
+                    'promoID' => (integer)$promo->promo_id,
+                    'type' => '',
+                    'photo' => $promo->path_image,
+                    'info' => $promo->description,
+                    'active' => $promo->status,
                     'title' => $promo->title,
-                    'date' => $promo->date,
+                    'date' => $promo->post_date,
                 ]);
             }
-            
-            return response()->json(ResponseCode::success($result_v2));
+
+            return response()->json(ResponseCode::success($result));
         }
     }
 }
